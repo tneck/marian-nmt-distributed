3561e2a76df3633ba836b561220a3b4754953b97
r0452425 <tomneckermann@gmail.com>
2017-07-09 19:17:15 +0100

    Implement MPI support

--- src/command/marian.cu
+++ src/command/marian.cu
@@ -1,3 +1,7 @@
+#if MPI_FOUND
+#include <mpi.h>
+#endif
+
 #include "marian.h"
 
 #include "models/model_task.h"
@@ -9,10 +13,30 @@ int main(int argc, char** argv) {
   auto options = New<Config>(argc, argv);
   auto devices = options->get<std::vector<size_t>>("devices");
 
-  if(devices.size() > 1)
-    WrapModelType<Train, AsyncGraphGroup>(options)->run();
-  else
-    WrapModelType<Train, SingletonGraph>(options)->run();
+  int comm_world_size = 0;
+  bool suitable_thread_mode;
+
+  #if MPI_FOUND
+  int provided_thread_mode = 0;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_thread_mode);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_world_size);
+  suitable_thread_mode = (provided_thread_mode >= MPI_THREAD_MULTIPLE);
+  #endif
+
+  if (comm_world_size > 1 && suitable_thread_mode) {
+    // Launch distributed asynch graph group
+    // @TODO: Launch DistAsyncGraphGroup
+  } else {
+    // Launch non-distributed graph group
+    if(devices.size() > 1)
+      WrapModelType<Train, AsyncGraphGroup>(options)->run();
+    else
+      WrapModelType<Train, SingletonGraph>(options)->run();
+  }
+
+  #if MPI_FOUND
+  MPI_Finalize();
+  #endif
 
   return 0;
 }
