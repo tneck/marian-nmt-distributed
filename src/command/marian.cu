1443b7c1c7988566c833908c678e480a76def720
r0452425 <tomneckermann@gmail.com>
2017-07-17 15:45:54 +0100

    Increase minimum MPI required thread mode to MPI_THREAD_MULTIPLE

--- src/command/marian.cu
+++ src/command/marian.cu
@@ -22,13 +22,17 @@ int main(int argc, char** argv) {
     int provided_thread_mode = 0;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided_thread_mode);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_world_size);
-    suitable_thread_mode = (provided_thread_mode >= MPI_THREAD_SERIALIZED);
+    suitable_thread_mode = (provided_thread_mode >= MPI_THREAD_MULTIPLE);
   }
   #endif
 
-  if (comm_world_size > 1 && suitable_thread_mode) {
-    LOG(info)->info("Launching Node Distributed Asynchronous Graph Group");
-    WrapModelType<Train, NodeDistAsyncGraphGroup>(options)->run();
+  if (comm_world_size > 1) {
+    if (suitable_thread_mode) {
+      LOG(info)->info("Launching Node Distributed Asynchronous Graph Group");
+      WrapModelType<Train, NodeDistAsyncGraphGroup>(options)->run();
+    } else {
+      LOG(info)->info("ERROR: No suitable MPI thread mode found. Required: MPI_THREAD_MULTIPLE. Please configure your MPI implementation appropriately. Aborting.");
+    }
   } else if(devices.size() > 1) {
     LOG(info)->info("Launching Asynchronous Graph Group");
     WrapModelType<Train, AsyncGraphGroup>(options)->run();
